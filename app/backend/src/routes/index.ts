22d34954881180486bf33ac66c54252248bb9865
rcamarano <rogeriocamarano@gmail.com>
2023-06-15 18:27:14 -0300

    create match route

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import teamsRouter from './teams.routes';
 import userRouter from './users.routes';
+import matchesRouter from './matches.routes';
 
 const router = Router();
 
 router.use('/teams', teamsRouter);
 router.use('/', userRouter);
+router.use('/matches', matchesRouter);
 
 export default router;
